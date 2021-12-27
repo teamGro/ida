751e0b2af36835fd3843eb126d4d748814dd0aa7
TimofeyG <tima.groshev@yandex.ru>
2021-12-28 00:33:44 +0300

    added product

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3000/';
 
 export default createStore({
   state: {
+    products: null,
   },
+  getters: {},
   mutations: {
+    saveProducts(state, products) {
+      state.products = products;
+    },
   },
   actions: {
-  },
-  modules: {
+    async getProducts({ commit }) {
+      const response = await axios.get(`${baseURL}api/products`);
+      commit('saveProducts', response.data);
+    },
   },
 });
