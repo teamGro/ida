bdd7d5ad84b2d2138d55aa9253f0cc3e16543265
TG <tima.groshev@yandex.ru>
2021-12-28 14:26:13 +0300

    added markup

---
+++ src/components/FormInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <label class="form__label" for=""
+      >{{ title }}<span class="form__required" v-if="isRequired"></span
+    ></label>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: ['title', 'isRequired'],
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.form {
+  &__label {
+    position: relative;
+    margin-bottom: 4px;
+
+    font-size: 10px;
+    line-height: 13px;
+    letter-spacing: -0.02em;
+    color: #49485e;
+  }
+
+  &__required {
+    position: absolute;
+    top: 0;
+    display: inline-block;
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+
+    background-color: #ff8484;
+  }
+}
+</style>
